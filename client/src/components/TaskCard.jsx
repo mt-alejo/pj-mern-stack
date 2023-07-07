9581d6099e1391daa1957914cea753543cf5b2a3
Ariel Alejandro <mt.calderon.alejandro@gmail.com>
2023-07-07 07:50:42 -0500

    Handle local storage [delete & toggle]

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,15 +1,23 @@
+/* eslint-disable react/prop-types */
 // import { deleteTaskRequest } from "../api/tasks.api";
-import { deleteLocalStorage } from "../hooks/useLocalStorage";
-function TaskCard({ task, id }) {
+import { handleLocalStorage } from "../hooks/useLocalStorage";
+function TaskCard({ tasks, task, id, setTasks }) {
   function handleDelete(id) {
     // deleteTaskRequest(id);
-    deleteLocalStorage(id);
+    //action, id, tasks, setTasks
+    handleLocalStorage("delete", id, tasks, setTasks);
+  }
+  function handleToggle() {
+    handleLocalStorage("toggle", id, tasks, setTasks);
+    // console.log(task.done);
   }
   return (
     <div>
       <h1>{task.title}</h1>
       <p>{task.description}</p>
-      <span>{task.done === 1 ? "✅" : "❌"}</span>
+      <span onClick={() => handleToggle(task.done)}>
+        {task.done === 1 ? "✅" : "❌"}
+      </span>
       <i>{task.createdAt}</i>
       <button onClick={() => handleDelete(id)}>Delete</button>
       <button>Edit</button>
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -4,10 +4,10 @@
 import { useEffect, useState } from "react";
 import TaskCard from "../components/TaskCard";
 import { useLocalStorage } from "../hooks/useLocalStorage";
+import { v4 as uuidv4 } from "uuid";
 
 function TasksPage() {
   const [tasks, setTasks] = useState([]);
-
   // useEffect(() => {
   //   async function loadTasks() {
   //     const response = await getAllTasksRequest();
@@ -16,16 +16,25 @@ function TasksPage() {
   //   loadTasks();
   // }, []);
   const response = useLocalStorage("TASKER");
-
   useEffect(() => {
     setTasks(response);
   }, []);
+  //Add the key value property:e
+  tasks.map((task) => (task.id = uuidv4()));
   return (
     <div>
       <h1>Tasks</h1>
-      {tasks?.map((task, index) => {
+      {tasks?.map((task) => {
         //The key must to be {key.id}
-        return <TaskCard key={index} id={index} task={task} />;
+        return (
+          <TaskCard
+            key={task.id}
+            id={task.id}
+            tasks={tasks}
+            setTasks={setTasks}
+            task={task}
+          />
+        );
       })}
     </div>
   );
